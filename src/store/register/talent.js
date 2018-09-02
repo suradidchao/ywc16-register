a9658823488b2a46b862d0aee222fcdf648dc8d0
Chicken <chill_shyld@hotmail.com>
2018-09-02 20:01:39 +0700

    extract required fields for talent page from backend user/me endpoints, and save the extracted fields into talent page store state

--- src/store/register/talent.js
+++ src/store/register/talent.js
@@ -1,3 +1,4 @@
+import { getSubsetObject } from '../../utils/helper'
 export default {
   state: {
     talent: {
@@ -7,37 +8,42 @@ export default {
     completed: false
   },
   mutations: {
-    setTalent (state, payload) {
-      state.talent = payload
+    setTalent(state, payload) {
+      const talentSchema = {
+        knowCamp: [],
+        activities: ''
+      }
+      const talentState = getSubsetObject(payload, talentSchema)
+      state.talent = talentState
     },
-    setKnowCamp (state, payload) {
+    setKnowCamp(state, payload) {
       state.talent.knowCamp = payload
     },
-    setActivities (state, payload) {
+    setActivities(state, payload) {
       state.talent.activities = payload
     },
-    clearKnowCamp (state) {
+    clearKnowCamp(state) {
       state.talent.knowCamp = null
     },
-    clearActivities (state) {
+    clearActivities(state) {
       state.talent.activities = null
     },
-    setCompleteTalent (state, payload) {
+    setCompleteTalent(state, payload) {
       state.completed = payload
     }
   },
   actions: {
-    completeTalent ({ commit }, payload) {
+    completeTalent({ commit }, payload) {
       commit('setCompleteTalent', payload)
     }
   },
   getters: {
-    talent (state) {
+    talent(state) {
       let talentState = { ...state }
       delete talentState.talent['completed']
       return talentState.talent
     },
-    talentCompleted (state) {
+    talentCompleted(state) {
       return state.completed
     }
   }
