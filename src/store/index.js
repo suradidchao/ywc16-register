48d3972819e0c8df326706f0984eed4eb646dcde
Suradid Chao <chill_shyld@hotmail.com>
2018-09-12 02:03:47 +0700

    clear vuex store on every time the app is created

--- src/store/index.js
+++ src/store/index.js
@@ -20,6 +20,87 @@ export const store = new Vuex.Store({
     generalQuestions: generalQuestions,
     majorQuestions: majorQuestions
   },
+  mutations: {
+    resetAppState (state, payload) {
+      const userState = {
+        data: {
+          firstName: '',
+          lastName: '',
+          firstNameEN: '',
+          lastNameEN: '',
+          completed_at: '',
+          major: '',
+          status: ''
+        }
+      }
+      const profileState = {
+        data: {
+          picture: '',
+          title: '',
+          firstName: '',
+          lastName: '',
+          firstNameEN: '',
+          lastNameEN: '',
+          nickname: '',
+          birthdate: '',
+          sex: '',
+          blood: '',
+          religion: '',
+          academicYear: '',
+          university: '',
+          faculty: '',
+          department: ''
+        },
+        complete: false
+      }
+      const contactInfoState = {
+        data: {
+          address: '',
+          province: '',
+          postalCode: '',
+          email: '',
+          phone: '',
+          emergencyName: '',
+          emergencyPhone: '',
+          emergencyPhoneRelated: '',
+          skype: '',
+          shirtSize: '',
+          disease: '',
+          food: '',
+          foodAllergy: ''
+        },
+        complete: false
+      }
+
+      const talentState = {
+        data: {
+          knowCamp: [],
+          activities: ''
+        },
+        completed: false
+      }
+
+      const generalQuestionsState = {
+        data: {
+          generalQuestions: []
+        },
+        complete: false
+      }
+
+      const majorQuestionsState = {
+        data: {
+          majorQuestions: []
+        },
+        complete: false
+      }
+      state.user.user = userState
+      state.profile.profileOne = profileState
+      state.profileTwo.profileTwo = contactInfoState
+      state.talent.talent = talentState
+      state.generalQuestions.generalQuestions = generalQuestionsState
+      state.majorQuestions.majorQuestions = majorQuestionsState
+    }
+  },
   getters: {
     summary (state) {
       return {
