542d43ea469425f6b4f442b76b7890572007501c
Suradid Chao <chill_shyld@hotmail.com>
2018-09-13 01:44:02 +0700

    complete auth guard, with redirect to missing page

--- src/router/auth-guard.js
+++ src/router/auth-guard.js
@@ -1,29 +1,52 @@
-import {store} from '../store'
+import { store } from '../store'
+
+let userInfoMissingAlert = (state) => {
+  let message = 'กรุณากรอกข้อมูลให้ครบถ้วนใน Step ดังนี้:  '
+  if (!state.profileCompleteOneStatus) {
+    message = message + '1,'
+  }
+  if (!state.profileTwoCompleteStatus) {
+    message = message + '2,'
+  }
+  if (!state.talentCompleteStatus) {
+    message = message + '3,'
+  }
+  if (!state.generalQuestionsCompleteStatus) {
+    message = message + '4,'
+  }
+  if (!state.majorQuestionsCompleteStatus) {
+    message = message + '5'
+  }
+  alert(message)
+}
+
+let navToFirstIncompletePage = (next, state) => {
+  if (!state.profileCompleteOneStatus) {
+    next('/steps/1')
+  } else if (!state.profileTwoCompleteStatus) {
+    next('/steps/2')
+  } else if (!state.talentCompleteStatus) {
+    next('/steps/3')
+  } else if (!state.generalQuestionsCompleteStatus) {
+    next('/steps/4')
+  } else if (!state.majorQuestionsCompleteStatus) {
+    next('/steps/5')
+  }
+}
 
 export default (to, from, next) => {
-  // const majorUser = store.getters.major
-  const profileOne = store.getters.profileOne
-  const profileTwo = store.getters.profileTwo
-  const generalQuestions = store.getters.generalQuestions
-  const talent = store.getters.talentCompleted
-  let majorQuestions = store.getters.completeMajorQuestions
-  // if (majorUser === 'design') {
-  //   majorQuestions = store.getters.designQuestions
-  // } else if (majorUser === 'marketing') {
-  //   majorQuestions = store.getters.marketingQuestions
-  // } else if (majorUser === 'content') {
-  //   majorQuestions = store.getters.contentQuestions
-  // } else if (majorUser === 'programming') {
-  //   majorQuestions = store.getters.programmingQuestions
-  // }
-  console.log('profileOne', profileOne.complete)
-  console.log('profileTwo', profileTwo.complete)
-  console.log('generalQuestions', generalQuestions.complete)
-  console.log('talent', talent)
-  console.log('majorQuestions', majorQuestions)
-  if (profileOne.complete === true && profileTwo.complete === true && talent === true && generalQuestions.complete === true && majorQuestions === true) {
+  let state = {
+    profileCompleteOneStatus: store.getters.profileOne.complete,
+    profileTwoCompleteStatus: store.getters.profileTwo.complete,
+    talentCompleteStatus: store.getters.talent.completed,
+    generalQuestionsCompleteStatus: store.getters.generalQuestions.complete,
+    majorQuestionsCompleteStatus: store.getters.majorQuestions.complete
+  }
+
+  if (state.profileCompleteOneStatus && state.profileTwoCompleteStatus && state.talentCompleteStatus && state.generalQuestionsCompleteStatus && state.majorQuestionsCompleteStatus) {
     next()
   } else {
-    next('/steps/1')
+    userInfoMissingAlert(state)
+    navToFirstIncompletePage(next, state)
   }
 }
