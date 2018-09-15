fa905b94490c73cc79d9f170b5c4c9598c06be59
Suradid Chao <chill_shyld@hotmail.com>
2018-09-15 13:03:15 +0700

    change http common from local to real api

--- src/core/http-common.js
+++ src/core/http-common.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const HTTP = axios.create({
-  baseURL: `http://localhost:3000/`
-  // baseURL: `http://api.ywc.in.th`
+  // baseURL: `http://localhost:3000/`
+  baseURL: `http://api.ywc.in.th`
 })
