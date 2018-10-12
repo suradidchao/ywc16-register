832cc5872872ec61df70a1bc90d65276c8a80af8
Suradid Chao <chill_shyld@hotmail.com>
2018-10-13 00:28:08 +0800

    add etc to educationStatus dropdown

--- src/core/http-common.js
+++ src/core/http-common.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const HTTP = axios.create({
-  // baseURL: `http://localhost:3000/`
+  // baseURL: `http://localhost:3001/`
   baseURL: `https://api.ywc.in.th`
 })
