d667cd23b2a06edcdea41d52f5d7f2eb23a6043d
Suradid Chao <chill_shyld@hotmail.com>
2018-10-19 17:33:22 +0700

    change api to production api

--- src/core/facebook.js
+++ src/core/facebook.js
@@ -1,23 +1,24 @@
 /* eslint-disable */
 export async function loadFbSdk() {
   return new Promise(resolve => {
-    window.fbAsyncInit = function() {
+    window.fbAsyncInit = function () {
       FB.init({
-        appId            : '715340261988670',
-        autoLogAppEvents : true,
-        xfbml            : true,
-        version          : 'v3.0'
+        appId: '134784707110171',
+        // appId            : '715340261988670',
+        autoLogAppEvents: true,
+        xfbml: true,
+        version: 'v3.0'
       });
       resolve('SDK Loaded!');
     };
 
-    (function(d, s, id){
-       var js, fjs = d.getElementsByTagName(s)[0];
-       if (d.getElementById(id)) {return;}
-       js = d.createElement(s); js.id = id;
-       js.src = "https://connect.facebook.net/en_US/sdk.js";
-       fjs.parentNode.insertBefore(js, fjs);
-     }(document, 'script', 'facebook-jssdk'));
+    (function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) { return; }
+      js = d.createElement(s); js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
   });
 }
 export function getFbLoginStatus() {
@@ -30,7 +31,7 @@ export function getFbLoginStatus() {
 
 export function fbLogin() {
   return new Promise(resolve => {
-    window.FB.login(response => resolve(response), {scope: 'email'});
+    window.FB.login(response => resolve(response), { scope: 'email' });
   });
 }
 export function fbLogout() {
